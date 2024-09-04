0afc36c786da99c7e458bd2a178647a21306f472
chairodrigues <74333285+chairodrigues@users.noreply.github.com>
2024-09-03 22:12:56 -0300

    boilerplate

---
+++ aspnet-core/src/ToDoList.EntityFrameworkCore/EntityFrameworkCore/ToDoListDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using ToDoList.Authorization.Roles;
+using ToDoList.Authorization.Users;
+using ToDoList.MultiTenancy;
+
+namespace ToDoList.EntityFrameworkCore
+{
+    public class ToDoListDbContext : AbpZeroDbContext<Tenant, Role, User, ToDoListDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
