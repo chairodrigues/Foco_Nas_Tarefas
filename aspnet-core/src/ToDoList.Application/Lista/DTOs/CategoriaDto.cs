01146a4af100471511f86e1467020501c283c48d
chairodrigues <74333285+chairodrigues@users.noreply.github.com>
2024-09-10 13:28:47 -0300

    Camada de Dto e map

---
+++ aspnet-core/src/ToDoList.Application/Lista/DTOs/CategoriaDto.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Lista.DTOs
+{
+    [AutoMap(typeof(Categoria))]
+    public class CategoriaDto:EntityDto<long>
+    {
+        public string CategoriaLista { get;set;}
+       public List<SubcategoriaDto> SubcategoriaLista { get;set;}
+
+        public string Descricao { get;set;}
+        public string Texto { get;set;}
+
+        public string Detalhamento { get;set;}
+    }
+}
